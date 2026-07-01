8d0cba2641588194d53f0b4468ed0c3be32e1ed0
PascalVZ96 <pascal@pascalvanzalk.nl>
2026-07-01 19:06:54 +0200

    Alpha 26: add dashboard entrypoint

---
+++ public/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+require dirname(__DIR__) . '/app/bootstrap.php';
+
+use MemoNetwork\Core\Auth;
+use MemoNetwork\Core\View;
+
+Auth::requireLogin();
+
+View::render('dashboard/index', [
+    'title' => 'Dashboard - MemoNetwork',
+    'user' => Auth::user(),
+]);
